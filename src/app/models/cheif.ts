c6ef06838bdfe01b8ca32fd17afd4a5edeb025a2
ranaelmallah <ranaelmallah88@gmail.com>
2024-11-07 20:57:19 +0200

    imgs commit

--- src/app/models/cheif.ts
+++ src/app/models/cheif.ts
@@ -2,7 +2,7 @@ export const Cheif=[
   {
     name:'Walter White',
     role:'Master Chef',
-    img:'../../assets/img/chefs/chefs-1.jpg',
+    img:'../../assets/img/chefs-1.jpg',
     facebook:'https://www.facebook.com/frasht.elwady.5?mibextid=ZbWKwL',
     instgram:'',
     linkedin:'https://www.linkedin.com/in/ranaelmallah/',
@@ -10,14 +10,14 @@ export const Cheif=[
   {
     name:'Sarah Jhonson',
     role:'Patissier',
-    img:'../../assets/img/chefs/chefs-2.jpg',
+    img:'../../assets/img/chefs-2.jpg',
     facebook:'https://www.facebook.com/frasht.elwady.5?mibextid=ZbWKwL',
     instgram:'',
     linkedin:'https://www.linkedin.com/in/ranaelmallah/',
   },{
     name:'William Anderson',
     role:'Cook',
-    img:'../../assets/img/chefs/chefs-3.jpg',
+    img:'../../assets/img/chefs-3.jpg',
     facebook:'https://www.facebook.com/frasht.elwady.5?mibextid=ZbWKwL',
     instgram:'',
     linkedin:'https://www.linkedin.com/in/ranaelmallah/',
--- src/app/models/gallary.ts
+++ src/app/models/gallary.ts
@@ -1,10 +1,10 @@
 export const Gallary=[
-  {img:'../../assets/img/gallery/gallery-1.jpg'},
-  {img:'../../assets/img/gallery/gallery-2.jpg'},
-  {img:'../../assets/img/gallery/gallery-3.jpg'},
-  {img:'../../assets/img/gallery/gallery-4.jpg'},
-  {img:'../../assets/img/gallery/gallery-5.jpg'},
-  {img:'../../assets/img/gallery/gallery-6.jpeg'},
-  {img:'../../assets/img/gallery/gallery-7.jpeg'},
-  {img:'../../assets/img/gallery/gallery-8.jpeg'},
+  {img:'../../assets/img/gallery-1.jpg'},
+  {img:'../../assets/img/gallery-2.jpg'},
+  {img:'../../assets/img/gallery-3.jpg'},
+  {img:'../../assets/img/gallery-4.jpg'},
+  {img:'../../assets/img/gallery-5.jpg'},
+  {img:'../../assets/img/gallery-6.jpeg'},
+  {img:'../../assets/img/gallery-7.jpeg'},
+  {img:'../../assets/img/gallery-8.jpeg'},
 ]
